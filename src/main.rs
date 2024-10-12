ac3ec7a5e723a331496ef3e81e775541c6d8bc87
Shadewalker3652 <jacobcarlson646@gmail.com>
2024-10-12 10:11:21 -0400

    Finished Parenthasis parsing, I was kind of messing around an dthen it workes so 👍

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ fn main() {
     let mut tokens: Vec<Token> = Vec::new();
     let mut lpars: Vec<usize> = Vec::new();
     let mut paren_sets: Vec<ParPairs> = Vec::new();
+    let mut asts: Vec<AST> = Vec::new();
 
     //static DOT: LazyLock<String> = LazyLock::new(|| String::from(".")); //OLD CODE FOR OLD DOT
 
@@ -111,11 +112,16 @@ fn main() {
 
             //P2: Adding a AST object
             let temp: Vec<Token> = tokens[paren_sets[paren_sets.len() - 1].l + 1.. paren_sets[paren_sets.len() - 1].r].to_vec();
-                for i in temp {
+                for i in &temp {
                     println!("Token: {} | Value: {}", i.token_type, i.value);
+                    tokens.remove(paren_sets[paren_sets.len() - 1].l + 1);
                 }
+
+                asts.push(AST {childern: temp});
+                tokens.insert(paren_sets[paren_sets.len() - 1].l + 1, Token {token_type: "AST".to_string(), value: (asts.len() - 1).to_string()});
                 println!("______________");
             }
+
    
         }
 
@@ -164,4 +170,8 @@ struct Token {
 struct ParPairs {
     l: usize,
     r: usize,
+}
+
+struct AST {
+    childern: Vec<Token>,
 }
